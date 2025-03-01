421db3f5f78f385d54b059f7df530dbf23eb1e1a
Kanthong Vannasaeng <kanthongv@gmail.com>
2025-03-01 16:10:38 -0300

    Some improvements

--- ingles.py
+++ ingles.py
@@ -15,15 +15,15 @@ z = "I have nothing to say. Normal so far."
 # File sentiment analysis
 import pandas as pd
 
-df = pd.read_csv("data/reviews.csv")
+dframe = pd.read_csv("data/reviews.csv")
 
-print(df.head())
+print(dframe.head())
 
 sid = SentimentIntensityAnalyzer()
-df["sentimiento"] = df["mensaje"].apply(lambda x: sid.polarity_scores(x)["compound"])
+dframe["sentimiento"] = dframe["mensaje"].apply(lambda x: sid.polarity_scores(x)["compound"])
 
 # Export to csv
-df.to_csv("salida-con-sentimiento.csv")
+df.to_csv("output/salida-con-sentimiento.csv")
 print(df)
 
 
